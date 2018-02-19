b8b6147a4b63de206b9605f2deddac7c80ee13fe
D.Snezhkov <dsnezhkov@gmail.com>
2018-02-19 09:52:51 -0600

    README

--- docs/run.sh
+++ docs/run.sh
@@ -9,12 +9,12 @@ AGENTPATH="../foxtrot.py"
 
 echo "============= Sending Data File=============="
 echo "Master: Sending data file"
-${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
+echo ${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
 	--nserver ${NSERVER} --domain ${DOMAIN} --role master \
 	--verbose info send --operation dfile --dfpath /tmp/datafile2
 
 echo "Slave: Processing request"
-${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
+echo ${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
 	--nserver ${NSERVER} --domain ${DOMAIN} --role slave \
 	--verbose info recv
 
@@ -25,63 +25,63 @@ echo "test" > /tmp/os.cmd2
 echo ">>>"
 cat /tmp/os.cmd2
 echo "<<<"
-${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
+echo ${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
 	--nserver ${NSERVER} --domain ${DOMAIN} --role master \
 	--verbose info send --operation ocmd --ofpath /tmp/os.cmd2
 
 echo "Slave: Processing request"
-${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
+echo ${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
 	--nserver ${NSERVER} --domain ${DOMAIN} --role slave \
 	--verbose info recv
 
 echo "Master: Processing response"
-${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
+echo ${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
 	--nserver ${NSERVER} --domain ${DOMAIN} --role master \
 	--verbose info recv
 
 echo "============= Sending OS Execution (command line) =============="
 echo "Master: Posting os exec() instructions"
-${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
+echo ${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
 	--nserver ${NSERVER} --domain ${DOMAIN} --role master  \
 	--verbose info send --operation ocmd --ocmd 'ps -ef | grep bash'
 
 echo "Slave: Showing raw record"
-${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
+echo ${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
 	--nserver ${NSERVER} --domain ${DOMAIN} --role slave \
 	--verbose info agent --operation show
 
 echo "Slave: Peeking at request"
-${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
+echo ${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
 	--nserver ${NSERVER} --domain ${DOMAIN} --role slave \
 	--verbose info agent --operation peek
 
 echo "Slave: Processing request"
-${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
+echo ${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
 	--nserver ${NSERVER} --domain ${DOMAIN} --role slave \
 	--verbose info recv
 
 echo "Master: Processing response"
-${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
+echo ${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
 	--nserver ${NSERVER} --domain ${DOMAIN} --role master \
 	--verbose info recv
 
 echo "Master: Peeking at response"
-${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
+echo ${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
 	--nserver ${NSERVER} --domain ${DOMAIN} --role slave \
 	--verbose info agent --operation peek
 
 echo "============= Misc =============="
 echo "Master: Resetting agent's instructions"
-${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
+echo ${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
 	--nserver ${NSERVER} --domain ${DOMAIN} --role master \
 	--verbose info agent --operation reset
 
 echo "Slave: Resetting agent's instructions"
-${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
+echo ${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
 	--nserver ${NSERVER} --domain ${DOMAIN} --role master \
 	--verbose info agent --operation reset
 
 echo "Slave: Identifying myself"
-${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
+echo ${AGENTPATH} --agent ${AGENT} --tsigname ${TSIGNAME} --tsigrdata ${TSIGRDATA} \
 	--nserver ${NSERVER} --domain ${DOMAIN} --role master \
 	--verbose info agent --operation ident
